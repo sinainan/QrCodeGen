faee55cb1f8b84890464586e7944b30ef7b6aa3e
Sina Inan <sinainan@gmail.com>
2020-10-22 14:31:43 +0200

    Final Working App
    Done

--- QrCodeGen/src/nl/finic/ui/Screen.java
+++ QrCodeGen/src/nl/finic/ui/Screen.java
@@ -3,16 +3,22 @@ package nl.finic.ui;
 import com.google.zxing.BarcodeFormat;
 import com.google.zxing.EncodeHintType;
 import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
 import com.google.zxing.client.j2se.MatrixToImageWriter;
 import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
 import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
 
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.IOException;
+import java.util.EnumMap;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -22,12 +28,13 @@ public class Screen extends JFrame{
     private JButton bGen;
     private JTextField tLink;
     private JButton bFile;
+    private JTextField tFile;
     private String choosertitle;
     private String path;
 
     public Screen() {
         add(jpScreen);
-        setSize(400, 200);
+        setSize(400, 400);
         setTitle("QrCodeGen - Finic");
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
@@ -44,8 +51,6 @@ public class Screen extends JFrame{
             @Override
             public void actionPerformed(ActionEvent e) {
                 createQr();
-                JOptionPane.showMessageDialog(null,"Qr-Code has been created","Done",1);
-                tLink.setText("");
             }
         });
 
@@ -68,10 +73,13 @@ public class Screen extends JFrame{
                             +  chooser.getCurrentDirectory());
                     System.out.println("getSelectedFile() : "
                             +  chooser.getSelectedFile());
+                    JOptionPane.showMessageDialog(null,"File Location Has Been Selected To : " + path,"File Location",1);
                 }
                 else {
                     System.out.println("No Selection ");
                 }
+
+
             }
         });
     }
@@ -79,19 +87,24 @@ public class Screen extends JFrame{
     public void createQr(){
         try {
             String link = tLink.getText();
-            //String qrCodeData = link;
-            String filePath =  path + "\\" +  link + ".png";
+            String name = tFile.getText();
+
+            String filePath =  path + "\\" +  name + ".png";
             String charset = "UTF-8"; // or "ISO-8859-1"
-            //path = filePath;
+
             Map<EncodeHintType, ErrorCorrectionLevel> hintMap = new HashMap< EncodeHintType, ErrorCorrectionLevel >();
             hintMap.put(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.L);
-            BitMatrix matrix = new MultiFormatWriter().encode(
-                    new String(link.getBytes(charset), charset),
-                    BarcodeFormat.QR_CODE, 200, 200, hintMap);
-            MatrixToImageWriter.writeToFile(matrix, filePath.substring(filePath
-                    .lastIndexOf('.') + 1), new File(filePath));
+
+            BitMatrix matrix = new MultiFormatWriter().encode(new String(link.getBytes(charset), charset), BarcodeFormat.QR_CODE, 200, 200, hintMap);
+            MatrixToImageWriter.writeToFile(matrix, filePath.substring(filePath.lastIndexOf('.') + 1), new File(filePath));
+
             System.out.println("QR Code image created successfully!");
             System.out.println(filePath);
+            System.out.println(name);
+
+            JOptionPane.showMessageDialog(null,"Qr-Code has been created","Done",1);
+            tLink.setText("");
+            tFile.setText("");
         } catch (Exception e) {
             System.err.println(e);
         }
